a44093d0c6ebadc2913cc12b0fef4aa14ebf2e63
Luan Moraes <moraes.luan1997@gmail.com>
2019-05-28 23:49:25 -0300

    updated

---
+++ SUPER@BROS.asm
@@ -0,0 +1,28 @@
+
+TITLE SUPER@BROS - JOGO BASEADO EM ASSEMBLY
+
+; Program Description:		Um jogo arcade ASCII desenvolvido na línguagem Assembly como requisito parcial
+;							para aprovação na disciplina Arquitetura e Organização de Computadores II
+;							da Universidade Federal de Sao Carlos, curso: Engenharia de Computacao
+;
+; Authors:					Luan V. Moraes	-	744342
+;							Antonio Caique	-	744332
+;
+; Creation Date:			28 de Maio de 2019
+
+INCLUDE Irvine32.inc	
+; other inclusions
+
+.DATA
+	; insert variables
+	
+
+.CODE
+main PROC
+	; instuctions
+	
+EXIT
+main ENDP
+	; insert additional procedures
+	
+END main 
