5f53d93dfb23909d9038bda5df099a5ac9fa1029
Luan Moraes <moraes.luan1997@gmail.com>
2019-06-26 22:47:46 -0300

    updated

--- src/variaveis.asm
+++ src/variaveis.asm
@@ -6,17 +6,17 @@ TITLE VARIAVEIS MODULO, SUPER @ BROS
 
 
 .DATA
-		PONTUACAO_TOTAL WORD 0					;guarda a soma da quantidade de bonus pegos em todas as telas
+		PONTUACAO_TOTAL 			WORD 0					;guarda a soma da quantidade de bonus pegos em todas as telas
 	
-		PONTUACAO BYTE "123456789"				;adaptacao para alterar display de pontos
+		PONTUACAO 					BYTE "123456789"		;adaptacao para alterar display de pontos
 
-		NIVEIS_CONCLUIDOS BYTE 0				;incrementa a cada nivel concluido
+		NIVEIS_CONCLUIDOS 			BYTE 0					;incrementa a cada nivel concluido
 		
-		TEMPO_TOTAL WORD 0						;a soma dos tempos gastos em cada mapa
+		TEMPO_TOTAL					WORD 0					;a soma dos tempos gastos em cada mapa
 	
-		PERSON DWORD 461						;guarda a posicao do personagem, inicialmente no nivel 1
+		PERSON 						DWORD 461				;guarda a posicao do personagem, inicialmente no nivel 1
 	
 	;manipulacao de tempo
-		POS_INICIAL_TIMER WORD 321				;local no mapa onde esta o primeiro elemento da barra de tempo
-		CONTAGEM_PROGRESSIVA BYTE 0				;de 0 a 30
-		COUNTER_CLOCK WORD 0					;contador auxiliar de tempo
+		POS_INICIAL_TIMER 			WORD 321				;local no mapa onde esta o primeiro elemento da barra de tempo
+		CONTAGEM_PROGRESSIVA 		BYTE 0					;de 0 a 30
+		COUNTER_CLOCK 				WORD 0					;contador auxiliar de tempo
